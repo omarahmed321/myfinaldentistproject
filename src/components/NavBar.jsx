2906dd6c456bdcf0cd6437692f62da6cdd2765c9
omarahmed321 <omarahmedgodabmox@gmail.com>
2026-08-17 19:00:55 +0300

    final?

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -36,7 +36,7 @@ setTitle("نظرة عامة")
 <div className='w-full py-2 px-2 md:py-4 md:px-8 flex justify-between  items-center bg-white border-b border-[#45556C]/20 text-[#0F172B] font-bold text-[20px]'>
   {/* left Side */}
      <div className="leftSide flex gap-3"> 
-    <p className=' text-[#45556C] bg-[#F1F5F9] md:text-[16px] p-2 rounded-full flex  justify-center items-center text-center text-[12px]  '>م ع</p>
+   <p className=' text-[#62748E] h-10 w-10 flex justify-center items-center bg-gray-200 rounded-full border border-white/30'>o</p>
         <Link href='/addpatient' className=' hover:transition-transform hover:scale-105 flex bg-[#0D9488]  text-white items-center rounded-md md:w-auto md:h-auto w-10 text-[12px]  md:py-2 md:px-4 md:text-[18px] h-8 font-semibold' > <span className='hidden md:flex'>إضافة مريض </span> <Plus className=' md:ml-2 mx-auto md:mx-0' /></Link>
         </div> 
         {/* right side */}
