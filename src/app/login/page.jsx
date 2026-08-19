768d6a0f46bd4b1c9dca5ed4fb59eb0e7f9ad5a7
omarahmed321 <omarahmedgodabmox@gmail.com>
2026-08-19 15:29:24 +0300

    finalGood

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -21,11 +21,11 @@ export  default async function page() {
 
 
 {/* the main login div */}
-   <div className="login p-10 rounded-xl bg-white border broder-[#E2E8F0] shadow-md text-end w-[90%] mx-auto md:w-[70%]">
+   <div className="login p-10 rounded-xl bg-white border border-[#E2E8F0] shadow-md text-end w-[90%] mx-auto md:w-[70%]">
     <p className='font-bold text-[20px] md:text-[24px] text-[#111827]' >مرحباً بك مجدداً</p>
     <p className='text-[14px] text-[#64748B]'>يرجى إدخال بياناتك للوصول إلى لوحة التحكم</p>
  {/* the fields are in another component cuz they use useformik/<Formik></Formik> */}
- <LoginBox />
+ <LoginBox  />
     {/* description */}
 <p className='text-[12px]  text-[#94A3B8] text-center h-10 flex flex-col justify-end border-t border-[#F1F5F9] '>© 2024 نظام إدارة عيادة الأسنان - الإصدار 1.0.0</p>
 </div>
